56b0558fd60e7f36ec6ac6d070447eb0f233d1bb
Gniewomir368 <61843466+kuba368@users.noreply.github.com>
2023-04-11 02:26:28 +0200

    Update 1.2

--- src/js/createPhotoCardMarkup.js
+++ src/js/createPhotoCardMarkup.js
@@ -8,24 +8,14 @@ const createPhotoCardMarkup = ({
   downloads,
 }) => {
   return `<div class="photo-card">
-  <a href=${largeImageURL}><img src="${webFormatURL}" alt="${tags}" loading="lazy" /></a>
+  <a href=${largeImageURL}>
+    <img src=${webFormatURL} alt="${tags}" loading="lazy" />
+  </a>
   <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-      <p>${likes}</p>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-      <p>${views}</p>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-      <p>${comments}</p>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-      <p>${downloads}</p>
-    </p>
+    <p class="info-item"><b>Likes</b>${likes}</p>
+    <p class="info-item"><b>Views</b>${views}</p>
+    <p class="info-item"><b>Comments</b>${comments}</p>
+    <p class="info-item"><b>Downloads</b>${downloads}</p>
   </div>
 </div>`;
 };
--- src/js/fetchPhotos.js
+++ src/js/fetchPhotos.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import { Notify } from 'notiflix';
-
-const API_KEY = '35276142-2bb78ec39400a28f997862a5f';
-const PHOTOS_PER_PAGE = 40;
+import { PHOTOS_PER_PAGE } from '../index';
 
 const fetchPhotos = async (name, page = 1) => {
-  const URL = `https://pixabay.com/api/?key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true&q=${name}&page=${page}&per_page=${PHOTOS_PER_PAGE}`;
+  const API_KEY = '35276142-2bb78ec39400a28f997862a5f';
   try {
-    const request = await axios.get(URL);
+    const request = await axios.get(
+      `https://pixabay.com/api/?key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true&q=${name}&page=${page}&per_page=${PHOTOS_PER_PAGE}`
+    );
     return request.data;
   } catch (error) {
     Notify.failure('ERROR', error);
